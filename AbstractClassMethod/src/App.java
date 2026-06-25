a6d6a30ab48a3ca5e1e38a7c6d6d1318239b9161
David Corrales <github.ferocity308@passmail.com>
2026-06-25 11:39:05 -0500

    abstract class

---
+++ AbstractClassMethod/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    // definición clase abstracta
+    abstract class Animal {
+
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
