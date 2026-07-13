22269831efe58cb6ada96ae420730380d4a8c730
David Corrales <github.ferocity308@passmail.com>
2026-07-13 09:01:08 -0500

    complete abstract class

--- AbstractClassMethod/src/App.java
+++ AbstractClassMethod/src/App.java
@@ -1,9 +1,34 @@
-public class App {
-    // definición clase abstracta
-    abstract class Animal {
+
+// definición clase abstracta
+abstract class Animal {
+
+    public abstract void makeSound();
+
+    public void sleep() {
+
+        System.out.println("Time to Sleep");
+    }
+
+}
+
+class Pig extends Animal {
+
+    public void makeSound() {
+
+        System.out.println("Oink pink");
 
     }
+
+}
+
+public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Animal myPig = new Pig();
+
+        myPig.makeSound();
+        myPig.sleep();
     }
+
 }
